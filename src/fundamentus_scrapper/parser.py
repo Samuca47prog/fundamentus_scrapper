2fcd6f47f97c355e6a428631ae4eeb9ff0bbe8cd
Samuca47prog <samuelsimao47@gmail.com>
2025-06-03 19:03:09 -0300

    parser functions to get table data

--- src/fundamentus_scrapper/parser.py
+++ src/fundamentus_scrapper/parser.py
@@ -1,6 +1,27 @@
 from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 import pandas as pd
 
+def get_page_soup(url: str) -> BeautifulSoup:
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(url,headers=hdr)
+    page = urlopen(req)
+    return BeautifulSoup(page)
+
+def get_table_from_soup(soup: BeautifulSoup) -> pd.DataFrame:
+  table = soup.find('table')
+
+  rows = []
+  for tr in table.find_all('tr'):
+      cells = [td.get_text(strip=True) for td in tr.find_all(['th', 'td'])]
+      rows.append(cells)
+
+  header = rows[0]
+  data = rows[1:]
+
+  return pd.DataFrame(data, columns=header)
+  
 def extract_table_from_html(html):
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find("table")
